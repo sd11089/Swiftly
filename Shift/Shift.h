80158eb02525135d5be26ee7891d9e1cba8fd736
Stephen Donnell <sdonnell.dev@gmail.com>
2015-01-11 16:51:59 -0600

    Initial Commit

---
+++ Shift/Shift.h
@@ -0,0 +1,19 @@
+//
+//  Shift.h
+//  Shift
+//
+//  Created by Stephen Donnell on 1/11/15.
+//  Copyright (c) 2015 Stephen Donnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shift.
+FOUNDATION_EXPORT double ShiftVersionNumber;
+
+//! Project version string for Shift.
+FOUNDATION_EXPORT const unsigned char ShiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shift/PublicHeader.h>
+
+
